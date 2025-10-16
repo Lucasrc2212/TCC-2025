3a582de3cfc7473b40ef99859122f0d470dea3ff
Lucas Rottini Cruz <162641853+Lucasrc2212@users.noreply.github.com>
2025-10-16 18:28:41 -0300

    Delete TCC 2025/portugues directory

--- TCC 2025/portugues/JS/introducao.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    usuarioLogado = localStorage.getItem("usuarioLogado");
-    if (usuarioLogado) {
-        document.getElementById("bemVindo").innerHTML = `Seja Bem-vindo(a), ${usuarioLogado}!`;
-    }
-}
